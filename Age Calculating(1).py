e57bea68e30166279a443587f4f1de8da2b42adb
Mohit <167441389+m-pharkya@users.noreply.github.com>
2024-07-23 20:20:14 +0530

    Add files via upload

---
+++ Age Calculating(1).py
@@ -0,0 +1,39 @@
+# Display a message asking the user to press enter
+input("Press Enter (2x) to continue...")
+
+# Check if the user pressed enter (the input will be an empty string)
+if input() == '':
+    print("Very Good")
+else:
+    print("jitna bola utna kar na be")
+
+# Ask for the user's age
+a = input("Enter your age: ")
+if a.isdigit():
+    number = int(a)
+    print("Now")
+
+else:
+    print("Age should be an integer ┬┬﹏┬┬")
+
+# Ask for the age gap between user and father
+b = input("Enter the age gap between you and your father: ")
+if b.isdigit():
+    number = int(b)
+    print("Your Father's age is:", number + int(a))
+else:
+    print("Age should be an integer ┬┬﹏┬┬")
+
+# Ask if the answer provided was correct
+d = input("Did I give the correct answer? (yes/no): ")
+if d.lower() == "yes":
+    print("Dekhaaaa, mujhe sab pata hai! :)")
+elif d.lower() == "no":
+    print("Mujhe bewakoof mat samajh ~_~")
+else:
+    print("Jitna bola utna kar")
+
+print ("Ab Program Khatam Ho Gya :) Bye Bye, Kisi Aur program me milenge")
+
+#developed by m_pharkya
+
